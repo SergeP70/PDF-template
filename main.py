7f4f02d8a996863b741ddd8f542a7dcb26f6b920
sergeP70 <serge@pille.be>
2025-01-03 13:05:55 +0100

    Add header

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ df = pd.read_csv('topics.csv')
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 for index, row in df.iterrows():
-    pdf.add_page()
-    print(row['Topic'], ":  ", row['Pages'])
+    for page in range(row['Pages']):
+        pdf.add_page()
+        print(row['Topic'], ":  ", row['Pages'])
 
-    pdf.set_font(family='Times', style='B', size=12)
-    pdf.cell(w=0, h=12, text=row['Topic'], align='L', new_x='LMARGIN', new_y='NEXT', border=1)
-    pdf.set_font(family='Helvetica', style='I', size=10)
-    pdf.cell(w=0, h=12, text='Pages: ' + str(row['Pages']) , align='L', new_x='LMARGIN', new_y='NEXT', border=1)
-
+        pdf.set_font(family='Times', style='B', size=24)
+        pdf.set_text_color(100,100,100) # grey
+        pdf.cell(w=0, h=12, text=row['Topic'], align='L', new_x='LMARGIN', new_y='NEXT')
+        pdf.line(10, 22, 200, 22 )
+        pdf.set_font(family='Helvetica', style='I', size=10)
+        pdf.cell(w=0, h=12, text='Pages: ' + str(row['Pages']) , align='L', new_x='LMARGIN', new_y='NEXT', border=0)
 
 pdf.output('output.pdf')
 
