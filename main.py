2c336588480ae17bc69c7b0a63aa7e060edfa178
sergeP70 <serge@pille.be>
2025-01-03 07:28:57 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# To create PDF documents
+from fpdf import FPDF
+import pandas as pd
+
+df = pd.read_csv('topics.csv')
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    print(row['Topic'], ":  ", row['Pages'])
+
+    pdf.set_font(family='Times', style='B', size=12)
+    pdf.cell(w=0, h=12, text=row['Topic'], align='L', new_x='LMARGIN', new_y='NEXT', border=1)
+    pdf.set_font(family='Helvetica', style='I', size=10)
+    pdf.cell(w=0, h=12, text='Pages: ' + str(row['Pages']) , align='L', new_x='LMARGIN', new_y='NEXT', border=1)
+
+
+pdf.output('output.pdf')
+
+
